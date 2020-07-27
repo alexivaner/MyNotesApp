3937dcfbb9d138cd0fa80bebcc72ef15a54cfc9e
Ivan Surya Hutomo <hutomoivan@gmail.com>
2020-07-27 12:00:04 +0800

    MyNotes app

--- app/src/main/java/com/example/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/com/example/mynotesapp/db/DatabaseContract.kt
@@ -1,17 +1,28 @@
 package com.example.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
-internal class DatabaseContract {
+object DatabaseContract {
 
+    const val AUTHORITY = "com.example.mynotesapp"
+    const val SCHEME = "content"
 
-    internal class NoteColumns : BaseColumns {
+    class NoteColumns : BaseColumns {
+
         companion object {
             const val TABLE_NAME = "note"
             const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+
+            // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
+
     }
 }
